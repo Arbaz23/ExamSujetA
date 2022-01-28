955b65ca59c69f0db8f6cb04f48f47aa7cea9a19
bhatti <bhattiarbazparis@gmail.com>
2022-01-28 15:53:17 +0100

    docker

--- testVehicule.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from Vehicule import Vehicule
-
-
-class TestPersonne(unittest.TestCase):
-    def test_personne_valid_ssn(self):
-        vehicule = Vehicule("Audi", "A4", "24", "grise", "NE-212-BA 91")
-        self.assertEqual(vehicule.vehiculevalid(), True)
